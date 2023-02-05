ef404d469bae52e54f679198486979c66889d1dd
DwaraknathT <dwarakasharma@gmail.com>
2023-02-05 14:18:23 +0000

    Basic cmake working

--- optimus/kernels/ops/gemm.cu
+++ optimus/kernels/ops/gemm.cu
@@ -1,22 +1,41 @@
 // GEneral Matrix Multiplication Kernel in CUDA C++ 
-
-#include "src/kernels/ops/gemm.h"
+#include "optimus/kernels/ops/gemm.h"
 
 namespace optimus {
     // A nested namespace for all the core math ops 
     namespace ops {
-
         // Cuda Kernel to add two N-D tensors of same shape. 
         // c[i] = a[i] + b[i] 
         template<typename T> 
-        __global__ void element_wise_add_kernel(T* a, T* b, T* c) {
-            
+        __global__ void GeMMKernel(T* a, T* b, T* result) {
+            *result = *a + *b;
         }
 
-        // Element wise addition of 2 tensors. 
-        template<typename T> 
-        void invokeGeMM(T* a, T* b, T* c) {
+        /* General matrix multiplication kernel. 
+            C = A * B 
+        To check if we can multiply A and B, the number of rows 
+        of A must match the number of columns of B. 
+        */
+        int InvokeGeMM(int a, int b) {
+            int c;
+            int *d_a, *d_b, *d_c; // device copies of a, b, c
+            int size = sizeof(int);
 
+            // Allocate space for device copies of a, b, c
+            cudaMalloc((void **)&d_a, size);
+            cudaMalloc((void **)&d_b, size);
+            cudaMalloc((void **)&d_c, size);
+            cudaMemcpy(d_a, &a, size, cudaMemcpyHostToDevice);
+            cudaMemcpy(d_b, &b, size, cudaMemcpyHostToDevice);
+            
+            // Launch add() kernel on GPU
+            GeMMKernel<<<1,1>>>(d_a, d_b, d_c);
+            // Copy result back to host
+            cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);
+            // Cleanup
+            cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+
+            return c;
         }
     }
 }
--- optimus/kernels/ops/gemm.h
+++ optimus/kernels/ops/gemm.h
@@ -3,10 +3,7 @@
 namespace optimus {
     // The following namespace holds all ops defined in optimus. 
     namespace ops {
-
-    // Element wise addition of two tensors.
-    template<typename T> 
-    void invokeAdd(T* operand_one, T* operand_two, T* result);
-
+        // Function to invoke a general matrix multiplication kernel.
+        int InvokeGeMM(int a, int b);
     }
 }
