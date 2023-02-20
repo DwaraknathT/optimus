219382812b608486fcdef90d82b3f740de984a27
DwaraknathT <dwarakasharma@gmail.com>
2023-02-20 10:03:40 +0000

    Some refactor

--- optimus/kernels/ops/gemm.cu
+++ optimus/kernels/ops/gemm.cu
@@ -7,7 +7,7 @@ namespace optimus {
 // A nested namespace for all the core math ops 
 namespace ops {
 
-template<typename T, const int chunk_size> 
+template<typename T, const int chunk_size, const int results_per_thread> 
 __global__ void GeMMKernel(T* A, T* B, T* C, 
                             const uint32_t M, 
                             const uint32_t N, 
@@ -24,26 +24,27 @@ __global__ void GeMMKernel(T* A, T* B, T* C,
     __shared__ T A_chunk[chunk_size][chunk_size];
     __shared__ T B_chunk[chunk_size][chunk_size];
 
-    T temp_dot_prods[chunk_size] = {0};
+    T dot_products[results_per_thread] = {0};
+    
     for (int chunk_idx = 0; chunk_idx < (((N - 1) / chunk_size) + 1); chunk_idx++) {
+        for (int current_result = 0; current_result < results_per_thread; current_result++) {
 
-        for (int current_thread_row = 0; current_thread_row < chunk_size; current_thread_row++) {
-            const int current_row = blockIdx.x * chunk_size + current_thread_row;
+            const int current_row = blockIdx.x * blockDim.x + current_result;
 
             if (current_row < M && (chunk_idx * chunk_size + thread_col) < N) {
                 const int A_index = (current_row * N) + (chunk_idx * chunk_size + thread_col);
-                A_chunk[current_thread_row][thread_col] = A[A_index];
+                A_chunk[current_result][thread_col] = A[A_index];
             }
             else {
-                A_chunk[current_thread_row][thread_col] = 0; 
+                A_chunk[current_result][thread_col] = 0; 
             }
             
-            if ((chunk_idx * chunk_size + current_thread_row) < N && (col < K)) {
-                const int B_index = (chunk_idx * chunk_size + current_thread_row) * K + col; 
-                B_chunk[current_thread_row][thread_col] = B[B_index];
+            if ((chunk_idx * chunk_size + current_result) < N && (col < K)) {
+                const int B_index = (chunk_idx * chunk_size + current_result) * K + col; 
+                B_chunk[current_result][thread_col] = B[B_index];
             }
             else {
-                B_chunk[current_thread_row][thread_col] = 0;
+                B_chunk[current_result][thread_col] = 0;
             }
         }
 
@@ -52,9 +53,8 @@ __global__ void GeMMKernel(T* A, T* B, T* C,
         if (row < M && col < K) {
             for (int i = 0; i < chunk_size; i++) {
                 T B_cache = B_chunk[i][thread_col];
-                for (int current_thread_row = 0; current_thread_row < chunk_size; current_thread_row++) {
-                    const int current_row = blockIdx.x * chunk_size + current_thread_row;
-                    temp_dot_prods[current_thread_row] += A_chunk[current_thread_row][i] * B_cache;
+                for (int current_result = 0; current_result < results_per_thread; current_result++) {
+                    dot_products[current_result] += A_chunk[current_result][i] * B_cache;
                 }
             }
         }
@@ -63,9 +63,9 @@ __global__ void GeMMKernel(T* A, T* B, T* C,
     } 
 
     if (row < M && col < K) {
-        for (int current_thread_row = 0; current_thread_row < chunk_size; current_thread_row++) {
-            const int current_row = blockIdx.x * chunk_size + current_thread_row;
-            C[current_row * K + col] = temp_dot_prods[current_thread_row];
+        for (int current_result = 0; current_result < results_per_thread; current_result++) {
+            const int current_row = blockIdx.x * blockDim.x + current_result;
+            C[current_row * K + col] = dot_products[current_result];
         }
     }
 }
@@ -81,10 +81,11 @@ void InvokeGeMM(T* A,
                 const float beta) {
     const int chunk_size = 32; 
     const int threads = 32;
+    const int results_pre_thread = 32;
     dim3 gridDim(div_ceil(M, chunk_size), div_ceil(K, chunk_size));
     dim3 blockDim(threads); 
     // Launch the kernel 
-    GeMMKernel<T, chunk_size><<<gridDim, blockDim>>>(A, B, C, M, N, K, alpha, beta);
+    GeMMKernel<T, chunk_size, results_pre_thread><<<gridDim, blockDim>>>(A, B, C, M, N, K, alpha, beta);
 }
 
 template void InvokeGeMM<int>(int* A,
